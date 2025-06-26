963e32392a0bcceccad401b358e7f9ac0745f95c
MvAlves <admin@freetofly.com.br>
2025-06-26 02:10:35 -0300

    Instalações das Dependencias de Compilação. Distro: Ubuntu 20.4

---
+++ building-dev.sh
@@ -0,0 +1,1 @@
+sudo apt install -y cmake build-essential libluajit-5.1-dev libmysqlclient-dev libboost-system-dev libboost-iostreams-dev libboost-filesystem-dev libpugixml-dev libcrypto++-dev libfmt-dev libboost-date-time-dev libgmp-dev
